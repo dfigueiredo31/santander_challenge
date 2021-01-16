c483f4941aee5772f6346f8bdf8e73518ea13ac8
MAGI-01\David Figueiredo <david.gabriel.31@gmail.com>
2021-01-16 22:09:36 +0000

    fontawesome

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace santander_challenge
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/fontawesome-all.css"));
         }
     }
 }
